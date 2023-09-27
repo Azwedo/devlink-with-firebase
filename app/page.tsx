532ea7ba6be2515e07e0e3370a97a22ef72b1fd0
Benjamin Fazli <bfzli@hotmail.com>
2023-09-27 15:13:42 +0200

    @bfzli | Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { FC, Fragment, ReactElement} from "react"
 import { PageHeaderInfo, PageLayout } from "@/ui/webflow"
-import { GlobalMetaTags } from "@/data"
 import { Metadata } from "next"
 
 const Page: FC = (): ReactElement => {
   return (
     <Fragment>
       <PageHeaderInfo title="Dashboard" />
-      <GlobalMetaTags title="Dashboard" />
 
       <PageLayout>
           {/* BUILD_AREA */}
